fa94ac4a6aaf615ee6268af1f6e7bfadbb96d714
Ben Coleman <ben@fffunction.co>
2018-01-19 19:27:02 +0000

    more work on gulp

--- assets/js/prototype.js
+++ assets/js/prototype.js
@@ -2,16 +2,16 @@ function showHideMenu(){
   var element = document.getElementById("navigation-primary");
 
   if (element.classList) {
-      element.classList.toggle("navigation-primary--active");
+    element.classList.toggle("navigation-primary--active");
   } else {
       // For IE9
       var classes = element.className.split(" ");
       var i = classes.indexOf("navigation-primary--active");
 
       if (i >= 0)
-          classes.splice(i, 1);
+        classes.splice(i, 1);
       else
-          classes.push("navigation-primary--active");
-          element.className = classes.join(" ");
+        classes.push("navigation-primary--active");
+        element.className = classes.join(" ");
   }
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,62 @@
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var connect     = require('gulp-connect');
-var livereload  = require('gulp-livereload');
-var concat      = require('gulp-concat');
-var uglify      = require('gulp-uglify');
-var rename      = require('gulp-rename');
-var imagemin    = require('gulp-imagemin');
-var cache       = require('gulp-cache');
+var cache			 = require('gulp-cache');
+var concat			= require('gulp-concat');
+var connect		 = require('gulp-connect');
+var gulp				= require('gulp');
+var imagemin		= require('gulp-imagemin');
+var livereload	= require('gulp-livereload');
+var rename			= require('gulp-rename');
+var sass				= require('gulp-sass');
+var uglify			= require('gulp-uglify');
 
 gulp.task('connect', function(){
-  connect.server({
-    root: 'public',
-    livereload: true
-  });
-});
-
-// keeps gulp from crashing for scss errors
-gulp.task('sass', function () {
-  return gulp.src('./assets/css/*.scss')
-      .pipe(sass({outputStyle: 'compressed',errLogToConsole: true }))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('./public/assets/css'))
-      .pipe(livereload());
-});
-
-gulp.task('scripts', function() {
-    return gulp.src('./assets/js/*.js')
-      .pipe(concat('prototype.js'))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(uglify())
-      .pipe(gulp.dest('./public/assets/js'))
-      .pipe(livereload());
-});
-
-gulp.task('images', function() {
-  return gulp.src('./assets/img/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('./public/assets/img'))
-    .pipe(livereload());
-});
-
-gulp.task('livereload', function (){
-  gulp.src('./public/**/*')
-  .pipe(connect.reload());
+	connect.server({
+		root: 'public',
+		livereload: true
+	});
 });
 
 gulp.task('watch', function () {
-  livereload.listen();
-  gulp.watch('./assets/css/**/*.scss', ['sass']);
-  gulp.watch('./assets/js/**/*.js', ['scripts']);
-  gulp.watch('./assets/img/**/*', ['images']);
-  gulp.watch('./public/**/*.html', ['livereload']);
+	livereload.listen();
+	gulp.watch('./assets/css/**/*.scss', ['sass']);
+	gulp.watch('./assets/js/**/*.js', ['scripts']);
+	gulp.watch('./assets/img/**/*', ['images']);
+	gulp.watch('./*.html', ['copy-index-html']);
 });
 
-gulp.task('default', ['connect', 'watch', 'sass', 'scripts', 'images']);
+gulp.task('sass', function () {
+	return gulp.src('./assets/css/*.scss')
+		.pipe(sass({outputStyle: 'compressed',errLogToConsole: true }))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('./public/assets/css'))
+		.pipe(livereload());
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('./assets/js/*.js')
+		.pipe(concat('prototype.js'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('./public/assets/js'))
+		.pipe(livereload());
+});
+
+gulp.task('images', function() {
+	return gulp.src('./assets/img/**/*')
+		.pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('./public/assets/img'))
+		.pipe(livereload());
+});
+
+gulp.task('copy-index-html', function() {
+	return gulp.src('./*.html')
+		.pipe(gulp.dest('./public'))
+		.pipe(livereload());
+		console.log('friee');
+});
+
+gulp.task('livereload', function (){
+	return gulp.src('./public/**/*')
+	 .pipe(connect.reload());
+});
+
+gulp.task('default', ['connect', 'watch', 'sass', 'scripts', 'images', 'copy-index-html']);
